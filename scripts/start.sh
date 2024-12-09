b427bb788a8187d10541fb3a2894e08b6153339d
Frieder Heugel <frieder.heugel@gmail.com>
2024-12-09 23:51:36 +0100

    Init

---
+++ scripts/start.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+AEM_JAR=$(find /aem/crx-quickstart/app/ -name "*quickstart*.jar")
+
+exec java \
+    -server \
+    -Djava.awt.headless=true \
+    -XX:+UseParallelGC \
+    -Xmx${JVM_XMX} \
+    -XX:MaxMetaspaceSize=${JVM_META} \
+    -agentlib:jdwp=transport=dt_socket,address=*:30303,server=y,suspend=n \
+    -XX:+JavaMonitorsInStackTrace \
+    -Dcom.sun.management.jmxremote.port=8686 \
+    -Dcom.sun.management.jmxremote.ssl=false \
+    -Dcom.sun.management.jmxremote.authenticate=false \
+    -Dcom.adobe.granite.crypto.file.disable=true \
+    -Dnashorn.args=--no-deprecation-warning \
+    -Dsling.run.modes=${AEM_RUNMODE} \
+    \
+    -jar ${AEM_JAR} \
+    \
+    start -c /aem/crx-quickstart -i launchpad \
+    -Dsling.properties=conf/sling.properties \
+    -a 0.0.0.0 -p 4500 \
+    >> /aem/crx-quickstart/logs/stdout.log
